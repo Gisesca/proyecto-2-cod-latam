94f5b8182d16b583562818b29799bda75e7f6359
Gisesca <mariagiselaescalona@gmail.com>
2023-06-28 14:45:17 -0300

    mi primer commit proyecto 2

---
+++ js/main.js
@@ -0,0 +1,35 @@
+window.addEventListener("scroll",function(){
+  var header # document. querySelector("header");
+  header.classList.toggle("abajo", this.window.scrollY> 0);
+}// Obtén todos los botones de categoría
+var botones = document.querySelectorAll('.categoria');
+
+// Agrega el evento 'click' a cada botón
+botones.forEach(function (boton) {
+  boton.addEventListener('click', function () {
+    // Elimina la clase 'activo' de todos los botones
+    botones.forEach(function (b) {
+      b.classList.remove('activo');
+    });
+
+    // Agrega la clase 'activo' solo al botón clicado
+    boton.classList.add('activo');
+  });
+});)
+
+
+
+
+
+document.getElementById('imagenes').addEventListener('mouseover', changeImage);
+document.getElementById('imagenes').addEventListener('mouseout', resetImage);
+
+function changeImage() {
+  var image = document.getElementById('imagenes');
+  image.src = "img/1.jpg"; // Reemplaza con la ruta de la nueva imagen
+}
+
+function resetImage() {
+  var image = document.getElementById('imagenes');
+  image.src = "img/2.jpg"; // Reemplaza con la ruta de la imagen original
+}

602c733131e2ec248798ed85dd3fef066429f01f
Gisesca <mariagiselaescalona@gmail.com>
2023-07-14 01:16:05 -0300

    commit 2

--- js/main.js
+++ js/main.js
@@ -1,37 +1,3 @@
-const nav = document.querySelector("#nav");
-const abrir = document.querySelector("#abrir");
-const cerrar = document.querySelector("#cerrar");
-
-abrir.addEventListener("click", () => {
-    nav.classList.add("visible");
-})
-
-cerrar.addEventListener("click", () => {
-    nav.classList.remove("visible");
-})
-
-
-
-
-window.addEventListener("scroll",function(){
-  var header # document. querySelector("header");
-  header.classList.toggle("abajo", this.window.scrollY> 0);
-}// Obtén todos los botones de categoría
-var botones = document.querySelectorAll('.categoria');
-
-// Agrega el evento 'click' a cada botón
-botones.forEach(function (boton) {
-  boton.addEventListener('click', function () {
-    // Elimina la clase 'activo' de todos los botones
-    botones.forEach(function (b) {
-      b.classList.remove('activo');
-    });
-
-    // Agrega la clase 'activo' solo al botón clicado
-    boton.classList.add('activo');
-  });
-});
-
 
 document.getElementById("imagenes").addEventListener('mouseover', changeImage);
 document.getElementById("imagenes").addEventListener('mouseout', resetImage);
@@ -43,5 +9,29 @@ function changeImage() {
 
 function resetImage() {
   var image = document.getElementById("imagenes");
-  image.src = "img/2.jpg"; // Reemplaza con la ruta de la imagen original
+  image.src = "img/3.jpg"; // Reemplaza con la ruta de la imagen original
 }
+
+// Funcionalidad Dark-Mode 
+function enableDarkMode() {
+  let main_body = document.body;
+  main_body.classList.toggle("dark-mode");
+
+  // Check if checkbox is checked or not
+  if (document.getElementById('checkitem').checked) {
+      console.log("checked");
+  } else {
+      console.log("NOT checked");
+  }
+
+
+}
+
+  const toggleButton = document.getElementsByClassName('toggle-button')[0]
+  const navBar = document.getElementsByClassName('nav-bar')[0]
+  
+  toggleButton.addEventListener('click', () => {
+    navBar.classList.toggle('active')
+  })
+
+  
